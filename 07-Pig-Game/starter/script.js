f67ae4a28a0530f4c96e6e6af4f41133be4be28e
Ellmagron <shikabane.developer@gmail.com>
2024-08-02 09:05:09 -0300

    Adicionando recurso de novo jogo, refatorando código e finalizando o projeto.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,6 +13,8 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
+
 //Funções
 let switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -22,15 +24,26 @@ let switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
-// Start Condidtions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let init = function () {
+  // Start Condidtions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true; //Varialvel de ambiente
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove(`player--active`);
+};
+
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -80,3 +93,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
