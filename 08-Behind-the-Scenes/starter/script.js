e9e48414453ed96d374547457a5976a328fd32fe
Ellmagron <shikabane.developer@gmail.com>
2024-08-05 20:16:59 -0300

    Escopo em prática

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function calcAge(birthYear) {
+  function printAge() {
+    const output = `${firstName}, you are ${age}, bor in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+
+      // Create NEW variable witg sane bane as outer scope's variable
+
+      const firstName = 'Pedro';
+
+      //Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `You're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+
+    console.log(millenial);
+    add(2, 3);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
